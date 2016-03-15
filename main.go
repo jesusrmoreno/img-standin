12591fab25164a9d894cb53ecb32aab65e8a6239
Jesus Moreno <jesusrmor94@gmail.com>
2016-03-15 12:38:39 -0400

    Cleaned up code

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 	"os"
@@ -17,95 +16,52 @@ import (
 const maxWidth = 10000
 const maxHeight = 10000
 
-type err interface {
-	error
-	Status() int
-}
-
-// StatusError represents an error with an associated HTTP status code.
-type statusError struct {
-	Code int
-	Err  error
-}
-
-// Allows StatusError to satisfy the error interface.
-func (se statusError) Error() string {
-	return se.Err.Error()
-}
-
-// Returns our HTTP status code.
-func (se statusError) Status() int {
-	return se.Code
-}
+// Sets the color of the text on the image
+const (
+	defaultTextColor   = "#ffffff"
+	defaultTextOutline = "#000000"
+)
 
 // Color Format Wrong, Please use Hex w/o hash symbol eg: ff0000
 func imageHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	hex, ok := vars["color"]
 	if !ok {
-		sErr := statusError{
-			Code: 400,
-			Err:  errors.New("Missing Color"),
-		}
-		http.Error(w, sErr.Error(), sErr.Status())
+		http.Error(w, "Missing Color", 400)
 		return
 	}
 	hex = "#" + hex
 	width, ok := vars["width"]
 	if !ok {
-		sErr := statusError{
-			Code: 400,
-			Err:  errors.New("Missing Width"),
-		}
-		http.Error(w, sErr.Error(), sErr.Status())
+		http.Error(w, "Missing Width", 400)
 		return
 	}
 	height, ok := vars["height"]
 	if !ok {
-		sErr := statusError{
-			Code: 400,
-			Err:  errors.New("Missing Height"),
-		}
-		http.Error(w, sErr.Error(), sErr.Status())
+		http.Error(w, "Missing Height", 400)
 		return
 	}
 
 	c, err := colorful.Hex(hex)
 	if err != nil {
-		sErr := statusError{
-			Code: 400,
-			Err:  errors.New("Invalid Color Format. Use HEX eg: ff0000"),
-		}
-		http.Error(w, sErr.Error(), sErr.Status())
+		http.Error(w, "Invalid Color, use hex eg: #ffffff", 400)
 		return
 	}
 
 	wi, err := strconv.Atoi(width)
 	if err != nil {
-		sErr := statusError{
-			Code: 400,
-			Err:  errors.New("Invalid Width"),
-		}
-		http.Error(w, sErr.Error(), sErr.Status())
+		http.Error(w, "Width must be a number", 400)
 		return
 	}
 	he, err := strconv.Atoi(height)
 	if err != nil {
-		sErr := statusError{
-			Code: 400,
-			Err:  errors.New("Invalid Height"),
-		}
-		http.Error(w, sErr.Error(), sErr.Status())
+		http.Error(w, "Height must be a number", 400)
 		return
 	}
 	if wi > maxWidth || he > maxHeight {
 		msg := fmt.Sprintf("Image too big must of size less than %dx%d",
 			maxWidth, maxHeight)
-		sErr := statusError{
-			Code: 400,
-			Err:  errors.New(msg),
-		}
-		http.Error(w, sErr.Error(), sErr.Status())
+		http.Error(w, msg, 400)
 		return
 	}
 	text, ok := vars["text"]
@@ -118,34 +74,27 @@ func imageHandler(w http.ResponseWriter, r *http.Request) {
 	_, withCross := vars["x"]
 	w.Header().Set("Content-Type", "image/jpeg")
 	if err := createImage(text, wi, he, c, withCross).EncodePNG(w); err != nil {
-		http.Error(w, err.Error(), 500)
+		http.Error(w, "Unable to create image", 500)
 		return
 	}
 }
 
-const fontSize = 24
-
-// if the r+g+b is > (256 / 2) * 3) -> lineColor = black else lineColor = white
-
-func createImage(text string, w, h int, bg colorful.Color, cross bool) *gg.Context {
+func createImage(text string, w, h int, bg colorful.Color, c bool) *gg.Context {
 	m := gg.NewContext(w, h)
 	m.SetColor(bg)
 	m.DrawRectangle(0, 0, float64(w), float64(h))
 	m.Fill()
-	red := bg.R
-	green := bg.G
-	blue := bg.B
-	if cross {
+	if c {
 		m.DrawLine(0, 0, float64(w), float64(h))
 		m.DrawLine(0, float64(h), float64(w), 0)
-		if red+green+blue > (.5 * 3) {
+		if bg.R+bg.G+bg.B > 1.5 {
 			m.SetRGBA(0, 0, 0, .7)
 		} else {
 			m.SetRGBA(1, 1, 1, .7)
 		}
 		m.Stroke()
 	}
-	m.SetHexColor("#000000")
+	m.SetHexColor(defaultTextOutline)
 	strokeSize := 2
 	for dy := -strokeSize; dy <= strokeSize; dy++ {
 		for dx := -strokeSize; dx <= strokeSize; dx++ {
@@ -154,7 +103,7 @@ func createImage(text string, w, h int, bg colorful.Color, cross bool) *gg.Conte
 			m.DrawStringAnchored(text, x, y, 0.5, 0.5)
 		}
 	}
-	m.SetHexColor("#ffffff")
+	m.SetHexColor(defaultTextColor)
 	m.DrawStringAnchored(text, float64(w)/2, float64(h/2), 0.5, 0.5)
 	m.Fill()
 	return m
